c61ac667a29ce5be8e7c84d7210e640238646c01
benjamin <benjamin.mccarthy.nz@gmail.com>
2025-01-03 10:22:49 +1300

    Completed chapter 10 - Partial Pre-rendering. This doesn't work unless we are using the latest canary version of NextJs so isn't relevant to this tutorial project

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
 
+// export const experimental_ppr = true; // Note - can only use this when using the latest canary version of Next.js
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // experimental: { // Note that we can only use this when using the latest canary version of Next.js
+  //   ppr: "incremental",
+  // },
 };
 
 export default nextConfig;
