b8079d6e0bc0a0f8d09ed33c27cbf8324d61dbe1
benjamin <benjamin.mccarthy.nz@gmail.com>
2025-01-03 13:46:54 +1300

    Completed chapter 13 - error handling

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -158,6 +158,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice); // Will be an empty array if the invoice wasn't found
     return invoice[0];
   } catch (error) {
     console.error("Database Error:", error);
